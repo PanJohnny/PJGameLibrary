aac032260ddd305fd5b75349dd984ed55e01180a
PanJohnny <devstefancz@gmail.com>
2022-11-08 20:45:38 +0100

    Added pom to git and removed unused stuff

--- src/main/java/com/panjohnny/pjgl/api/object/GameObjectFactory.java
+++
@@ -1,7 +0,0 @@
-package com.panjohnny.pjgl.api.object;
-
-@SuppressWarnings("unused")
-@FunctionalInterface
-public interface GameObjectFactory<T extends GameObject> {
-    T create(int x, int y, int width, int height);
-}
--- src/main/java/com/panjohnny/pjgl/api/object/GameObjectRegistry.java
+++
@@ -1,8 +0,0 @@
-package com.panjohnny.pjgl.api.object;
-
-import com.panjohnny.pjgl.api.utils.Identifier;
-import com.panjohnny.pjgl.api.utils.AbstractRegistry;
-
-@SuppressWarnings("unused")
-public class GameObjectRegistry<T extends GameObject> extends AbstractRegistry<Identifier, GameObjectFactory<T>> {
-}
