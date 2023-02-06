9521cb8de46afa6f2bfa1b125d915001f7586e36
PanJohnny <devstefancz@gmail.com>
2023-02-06 21:43:17 +0100

    Directional error

--- src/main/java/com/panjohnny/pjgl/api/object/components/Position.java
+++ src/main/java/com/panjohnny/pjgl/api/object/components/Position.java
@@ -92,9 +92,9 @@ public class Position extends Component {
     }
 
     public enum Direction {
-        UP(0, 1),
+        UP(0, -1),
         RIGHT(1, 0),
-        DOWN(0, -1),
+        DOWN(0, 1),
         LEFT(-1, 0),
         UP_RIGHT(1, -1),
         UP_LEFT(-1, -1),
