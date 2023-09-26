35e0fc36c8e1a881657aa975c3a51125ef86c715
PanJohnny <devstefancz@gmail.com>
2023-09-26 20:40:57 +0200

    Made the project modular

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+open module com.panjohnny.pjgl {
+    requires java.desktop;
+    requires org.lwjgl;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.opengl;
+    requires com.google.gson;
+
+    exports com.panjohnny.pjgl.adapt;
+    exports com.panjohnny.pjgl.adapt.desktop;
+    exports com.panjohnny.pjgl.adapt.lwjgl;
+
+    exports com.panjohnny.pjgl.api.asset.atlas;
+    exports com.panjohnny.pjgl.api.asset;
+    exports com.panjohnny.pjgl.api.camera;
+    exports com.panjohnny.pjgl.api.event;
+    exports com.panjohnny.pjgl.api.object.components;
+    exports com.panjohnny.pjgl.api.object;
+    exports com.panjohnny.pjgl.api.util;
+    exports com.panjohnny.pjgl.api;
+
+    exports com.panjohnny.pjgl.core;
+    exports com.panjohnny.pjgl.core.adapters;
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,3 @@
+module PJGLTest {
+    requires com.panjohnny.pjgl;
+}
